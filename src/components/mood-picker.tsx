3ab7756f0c8b19fe80f312c20fbabfe3462a32af
jmamadeu <jmamadeu2000@gmail.com>
2021-11-25 00:59:11 +0100

    chore: add style to mood

--- src/components/mood-picker.tsx
+++ src/components/mood-picker.tsx
@@ -1,6 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { theme } from '../styles/theme';
+import { MoodOptionType } from '../types/moods';
 
-const moodOptions = [
+const moodOptions: MoodOptionType[] = [
   { emoji: '🧑‍💻', description: 'studious' },
   { emoji: '🤔', description: 'pensive' },
   { emoji: '😊', description: 'happy' },
@@ -8,20 +11,90 @@ const moodOptions = [
   { emoji: '😤', description: 'frustrated' }
 ];
 
-export function MoodPicker() {
+export const MoodPicker: React.FC = () => {
+  const [selectedMood, setSelectedMood] = React.useState<MoodOptionType>();
+
   return (
-    <View style={styles.moodOptions}>
-      {moodOptions.map((mood, index) => (
-        <Text key={index}>{mood.emoji}</Text>
-      ))}
+    <View style={styles.container}>
+      <Text style={styles.heading}>How are you right now?</Text>
+      <View style={styles.moodList}>
+        {moodOptions.map((option, index) => (
+          <View key={index}>
+            <Pressable
+              onPress={() => setSelectedMood(option)}
+              key={option.emoji}
+              style={[
+                styles.moodItem,
+                option.emoji === selectedMood?.emoji ? styles.selectedMoodItem : undefined
+              ]}
+            >
+              <Text style={styles.moodText}>{option.emoji}</Text>
+            </Pressable>
+            <Text style={styles.descriptionText}>
+              {selectedMood?.emoji === option.emoji ? option.description : ' '}
+            </Text>
+          </View>
+        ))}
+      </View>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Choose</Text>
+      </Pressable>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  moodOptions: {
+  moodText: {
+    fontSize: 24
+  },
+  moodList: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: 20
+    justifyContent: 'space-between'
+  },
+  moodItem: {
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+    marginBottom: 5
+  },
+  selectedMoodItem: {
+    borderWidth: 2,
+    backgroundColor: theme.colors.purple,
+    borderColor: theme.colors.white
+  },
+  descriptionText: {
+    color: theme.colors.purple,
+    fontWeight: 'bold',
+    fontSize: 10,
+    textAlign: 'center'
+  },
+  container: {
+    borderWidth: 2,
+    borderColor: theme.colors.purple,
+    margin: 10,
+    borderRadius: 10,
+    padding: 20
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    letterSpacing: 1,
+    textAlign: 'center',
+    marginBottom: 20
+  },
+  button: {
+    backgroundColor: theme.colors.purple,
+    width: 150,
+    borderRadius: 20,
+    marginTop: 20,
+    alignSelf: 'center',
+    padding: 10
+  },
+  buttonText: {
+    color: theme.colors.white,
+    textAlign: 'center',
+    fontWeight: 'bold'
   }
 });
---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+const COLORS = {
+  purple: '#454c73',
+  white: '#fff'
+};
+
+export const theme = {
+  colors: COLORS
+};
